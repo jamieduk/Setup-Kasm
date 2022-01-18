3f6b9e91fa40bb175d8cd7b6e1dc1e4b7eacdb95
Jay <jamied_uk@hotmail.com>
2022-01-18 01:47:01 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-wget https://github.com/kasmtech/KasmVNC/releases/download/v0.9.2-beta/kasmvncserver_ubuntu_bionic_0.9.2_amd64.deb
+echo "Setting Up Kasm"
+echo ""
+sudo dd if=/dev/zero bs=1M count-1024 of=/mnt/1GiB.swap
+sudo chmod 600 /mnt/1GiB.swap
+sudo mkswap /mnt/1GiB.swap
+echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
-sudo dpkg -i kasmvncserver_*.deb
-sudo apt-get -f install
-
-# We provide an example script to run KasmVNC at #
-# /usr/share/doc/kasmvncserver/examples/kasmvncserver-easy-start. It runs a VNC
-# server on display :10 and on interface 0.0.0.0. If you're happy with those
-# defaults you can just use it as is:
-sudo ln -s /usr/share/doc/kasmvncserver/examples/kasmvncserver-easy-start /usr/bin/
-
-# Add your user to the ssl-cert group
-sudo addgroup $USER ssl-cert
-# You will need to re-connect in order to pick up the group change
-
-# Create ~/.vnc directory and corresponding files.
-kasmvncserver-easy-start -d && kasmvncserver-easy-start -kill
-
-# Modify vncstartup to launch your environment of choice, in this example LXDE
-# This may be optional depending on your system configuration
-echo '/usr/bin/lxsession -s LXDE &' >> ~/.vnc/xstartup
-
-# Start KasmVNC with debug logging:
-kasmvncserver-easy-start -d
-
-# Tail the logs
-tail -f ~/.vnc/`hostname`:10.log
-
-Now navigate to your system at https://[ip-address]:8443/
-
-To stop a running KasmVNC:
-
-kasmvncserver-easy-start -kill
+wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
+tar -xf kasm_release*.tar.gz
+sudo bash kasm_release/install.sh
