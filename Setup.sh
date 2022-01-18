15f492fd8d2b428e97cd519fd989fc4c69a4691a
Jay <jamied_uk@hotmail.com>
2022-01-18 12:10:43 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -9,4 +9,10 @@ echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
 wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
 tar -xf kasm_release*.tar.gz
-sudo bash kasm_release/install.sh
+echo "Which port you want server to run on?"
+read port
+sudo bash kasm_release/install.sh -L $port
+
+# If you would like to run the Web Application on a different port pass
+# the -L flag when calling the installer. e.g 
+# sudo bash kasm_release/install.sh -L 8443
