a7407c85765df164641d56c8e5393800c73b24fc
Jay <jamied_uk@hotmail.com>
2022-01-18 18:15:22 +0000

    Update Disable_appcrash_reporting.sh

--- Disable_appcrash_reporting.sh
+++ Disable_appcrash_reporting.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Change 1 to 0 and save and exit"
+echo "Change 1 to 0 and save and exit, enter to continue..."
+read Y
 sudo nano /etc/default/apport
 sudo systemctl disable apport
 sudo apt purge apport
