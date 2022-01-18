e04f4bc6cfbcce03b87eb63aa72edd79ebbe7713
Jay <jamied_uk@hotmail.com>
2022-01-18 02:01:15 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -4,6 +4,7 @@ echo ""
 sudo dd if=/dev/zero bs=1M count-1024 of=/mnt/1GiB.swap
 sudo chmod 600 /mnt/1GiB.swap
 sudo mkswap /mnt/1GiB.swap
+sudo swapon /mnt/1GiB.swap
 echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
 wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
